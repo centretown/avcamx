9d7ba80a42fe6b92fcce6029de3fb79505c5ff10
Centretown <centretown77@gmail.com>
2026-02-13 07:47:11 -0500

    added device monitor to enable device detection

--- v4lweb/v4lweb.go
+++ v4lweb/v4lweb.go
@@ -4,27 +4,41 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/centretown/avcamx"
 )
 
 func main() {
-	host := avcamx.NewAvHost("", "")
-	host.MakeLocal(nil)
+	avFlags := avcamx.NewAvFlags()
+	exists := avFlags.HasFile()
+	if exists {
+		avFlags.Load()
+	}
 
-	httpErr := make(chan error, 1)
-	go func() {
-		httpErr <- host.ListenAndServe()
-	}()
+	avFlags.Parse()
+	err := avFlags.Save()
+	if err != nil {
+		log.Printf("Error saving configuration file %s. %s", avcamx.ConfigName, err)
+	} else if exists {
+		log.Print("Saved configuration file. ", avcamx.ConfigName)
+	} else {
+		log.Print("Created configuration file. ", avcamx.ConfigName)
+	}
+
+	avFlags.Print()
+
+	host := avcamx.NewAvHost(avFlags.HostAddr, avFlags.HostPort, avFlags.Remotes)
+	done := make(chan int)
+	go host.Monitor(done)
+
+	log.Printf("\nServing %s...", host.Url)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt)
-	select {
-	case err := <-httpErr:
-		log.Printf("failed to serve http: %v", err)
-	case sig := <-sigs:
-		log.Printf("terminating: %v", sig)
-	}
-
+	sig := <-sigs
+	log.Printf("Interrupted: %v", sig)
+	done <- 1
+	time.Sleep(3 * time.Second)
 	host.Quit()
 }
